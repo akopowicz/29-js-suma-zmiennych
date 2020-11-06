97e4fef699a52eeb27cf7626c43b1c2ad49f3609
akopowicz <anna.kopowicz@gmail.com>
2020-11-06 18:37:54 +0100

    poprawione

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 let firstNumber = 12;
 let secondNumber = 36;
 
-function sum() {
+function sum(num1, num2) {
     suma = firstNumber + secondNumber;
     return suma;
 }
 
-console.log(sum());
+console.log(sum(firstNumber, secondNumber));
